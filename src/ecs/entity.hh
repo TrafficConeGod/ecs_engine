2f19c349e92f2eca54fd729eee1c9ad7f16306ba
TrafficConeGod <TrafficConeGod@github.com>
2022-02-08 11:38:29 -0500

    make db a passed reference into entity

--- src/ecs/entity.hh
+++ src/ecs/entity.hh
@@ -6,22 +6,19 @@
 
 namespace newt::ecs {
     class entity {
-        database* db;
         std::size_t index = 0;
         
         std::array<std::size_t, MAX_COMPONENTS> component_indices = {0};
 
         template<typename C>
-        inline const C& get_component_impl() const {
+        inline const C& get_component_impl(database& db) const {
             if (!has_component<C>()) {
                 throw std::runtime_error("no component found");
             }
             // We subtract one since the index stored must be greater than 0
-            return db->get_component_set<C>().at(component_indices.at(C::ID) - 1);
+            return db.get_component_set<C>().at(component_indices.at(C::ID) - 1);
         }
         public:
-            inline entity(database& db_) : db(&db_) {}
-
             inline std::size_t get_index() const { return index; }
             inline void set_index(std::size_t index) { this->index = index; }
 
@@ -34,34 +31,34 @@ namespace newt::ecs {
 
             // Not inherently thread safe
             template<typename C>
-            inline const C& get_component() const {
-                return get_component_impl<C>();
+            inline const C& get_component(database& db) const {
+                return get_component_impl<C>(db);
             }
 
             // Not inherently thread safe
             template<typename C>
-            inline C& get_component() {
-                return const_cast<C&>(get_component_impl<C>());
+            inline C& get_component(database& db) {
+                return const_cast<C&>(get_component_impl<C>(db));
             }
 
             // Not inherently thread safe
             template<typename C>
-            void set_component(const C& component) {
+            void set_component(database& db, const C& component) {
                 if (has_component<C>()) {
                     throw std::runtime_error("component already exists");
                 }
                 // We add one since the index stored must be greater than 0
-                component_indices.at(C::ID) = db->get_component_set<C>().insert(component) + 1;
+                component_indices.at(C::ID) = db.get_component_set<C>().insert(component) + 1;
             }
 
             // Not inherently thread safe
             template<typename C>
-            void erase_component() {
+            void erase_component(database& db) {
                 if (!has_component<C>()) {
                     throw std::runtime_error("no component found");
                 }
                 // We subtract one since the index stored must be greater than 0
-                db->get_component_set<C>().erase_at(component_indices.at(C::ID) - 1);
+                db.get_component_set<C>().erase_at(component_indices.at(C::ID) - 1);
                 // Resetting to the default value
                 component_indices.at(C::ID) = 0;
             }
--- src/engine/main.cc
+++ src/engine/main.cc
@@ -13,9 +13,9 @@ int main() {
     auto& e = db.entity_set.at(entity_index);
 
     std::lock_guard lock2(db.core_set.mutex);
-    e.set_component<components::core>({ .val = 20 });
-    e.erase_component<components::core>();
-    e.set_component<components::core>({ .val = 20 });
+    e.set_component<components::core>(db, { .val = 20 });
+    e.erase_component<components::core>(db);
+    e.set_component<components::core>(db, { .val = 20 });
 
     // two ways of iterating through components
     for (auto& c : db.core_set) {
@@ -23,7 +23,7 @@ int main() {
         c.val = 30;
     }
     for (auto& e : db.entity_set) {
-        std::printf("%zu\n", e.get_component<components::core>().val);
+        std::printf("%zu\n", e.get_component<components::core>(db).val);
     }
 
     return 0;
