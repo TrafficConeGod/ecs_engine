3aef42019dc93184c5e843fa78f4fb65e061f8fe
TrafficConeGod <TrafficConeGod@github.com>
2022-02-08 11:38:50 -0500

    remove unecessary name

--- src/ecs/database.cc
+++ src/ecs/database.cc
@@ -5,7 +5,7 @@ using namespace newt::ecs;
 
 std::size_t database::create_entity() {
     // We have to assign the index to the entity after it has been created
-    auto index = entity_set.insert(entity{});
+    auto index = entity_set.insert({});
     entity_set.at(index).set_index(index);
     return index;
 }
